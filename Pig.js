bc3462a6824f1aee030281210adb37274c59cb24
ysahoo78 <70029595+ysahoo78@users.noreply.github.com>
2020-10-05 14:01:05 -0400

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,22 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.visibilty = 255;
+  }
+  display() { 
+    //console.log(this.body.speed);
+    if (this.body.speed < 3) {
+      super.display();
+    }
+    else {
+      push();
+      this.visibilty = this.visibility - 5;
+      tint(255,this.visibility);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+      World.remove(world, this.body);
+      pop();
+    }
+    
+  }
+}
